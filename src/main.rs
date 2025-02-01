637744cf14e72dad2d4a8ab09b54663e73100fa5
Franklin Diaz <franklin83diaz@gmail.com>
2025-02-01 16:42:36 -0500

    app example ui setting

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+slint::include_modules!();
+use device_query::{DeviceQuery, DeviceState};
+use slint::{ComponentHandle, LogicalPosition, Window};
+
+
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
+  
+    // main_window.show();
+    let app_handle = main_window.as_weak();
+    main_window.on_move_windows(move || {
+        if let Some(main_window) = app_handle.upgrade() {
+            let device_state = DeviceState::new();
+            let mouse = device_state.get_mouse();
+            let (x, y) = mouse.coords;
+            let x =x -15;
+            let y = y-15;
+
+            main_window
+                .window()
+                .set_position(LogicalPosition::new(x as f32, y as f32));
+        }
+    });
+
+
+
+    //  main_window.window().set_position(LogicalPosition::new(0.0, 0.0));
+    main_window.run()
+}
