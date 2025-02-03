79b9c4c8bf8b5b79d7bce7951618095b0c8c8edc
Franklin Diaz <franklin83diaz@gmail.com>
2025-02-03 17:20:01 -0500

    Key Stop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 slint::include_modules!();
 use device_query::{DeviceQuery, DeviceState};
 use slint::{ComponentHandle, LogicalPosition};
+use rdev::{listen, Event, EventType};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 fn main() -> Result<(), slint::PlatformError> {
@@ -22,5 +24,36 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
+    // if let Err(error) = listen(callback) {
+    //     eprintln!("Error: {:?}", error);
+    // }
+
     main_window.run()
+}
+
+fn callback(event: Event) {
+    println!("{:?}", event);
+    
+    let start = SystemTime::now();
+    let millis = start.duration_since(UNIX_EPOCH)
+        .expect("El tiempo debe ser posterior a UNIX_EPOCH")
+        .as_millis();
+    
+  
+    match event.event_type {
+
+        EventType::KeyPress(rdev::Key::ControlLeft) => {
+            println!("Left Control");
+        }
+        EventType::MouseMove { x, y } => {
+            println!("Mouse moved to: x = {},y = {} , time = {}", x, y, millis);
+        }
+        EventType::ButtonPress(button) => {
+            println!("Mouse button pressed: {:?}", button);
+        }
+        EventType::ButtonRelease(button) => {
+            println!("Mouse button released: {:?}", button);
+        }
+        _ => {}
+    }
 }
